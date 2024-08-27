4d7ef8910d42ed8adcd93b7116a86d00a02d205d
Guilherme <guilherme.silva@develcode.com.br>
2024-08-27 11:32:00 -0300

    Header Component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,1 +1,23 @@
-// Componentes relacionados ao layout da aplicação, como cabeçalhos e rodapés. 
+import { Outlet } from "react-router-dom"
+import styled from "styled-components"
+import { Header } from "../Header"
+
+
+const ContentWrapper = styled.div`
+  margin-left: 250px;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 16px;
+`
+
+export const LayoutLogged = () => {
+    return (
+        <>
+            <Header />
+            <ContentWrapper>
+                <Outlet />
+            </ContentWrapper>
+        </>
+    )
+}
