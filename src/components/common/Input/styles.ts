83b1e6a0ad91d09464cb980ef51b7bd2db6d4c9f
Guilherme <guilherme.silva@develcode.com.br>
2024-08-30 14:26:14 -0300

    styles

--- src/components/common/Input/styles.ts
+++ src/components/common/Input/styles.ts
@@ -28,14 +28,15 @@ export const StyledInput = styled.input`
     padding: .719rem;
     width:100%;
     color:${({ theme }) => theme.colors.inputTextColor};
-
+    letter-spacing:.04rem;
+    font-size:1.13rem;
     &::placeholder{
         color:${({ theme }) => theme.colors.darkGray};
+        letter-spacing:0.04rem;
     }
 
     @media(max-width:600px){
         &::placeholder{
-        letter-spacing:0.05rem;
     }
     }
 `;
